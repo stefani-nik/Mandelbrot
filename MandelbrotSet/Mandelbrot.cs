6f4bc9c0b0abf28707c4cfb291a68941bff978e5
stefani-nik <stefani_nik@abv.bg>
2018-04-01 22:22:28 +0300

    Refactoring

--- MandelbrotSet/Mandelbrot.cs
+++ MandelbrotSet/Mandelbrot.cs
@@ -27,31 +27,21 @@ namespace MandelbrotSet
             Bitmap bm = new Bitmap(width, height);
 
 
-            double xs = (posX - rangeStart / 2.0);
-            double ys = (posY - rangeEnd / 2.0);
-            double xd = rangeStart / (double)width;
-            double yd = rangeEnd / (double)height;
+            double xStartPos = (posX - rangeStart / 2.0);
+            double yStartPos = (posY - rangeEnd / 2.0);
+            double xOffset = rangeStart / (double)width;
+            double yOffset = rangeEnd / (double)height;
 
-            double y1 = ys + yd;
+            double coordY = yStartPos + yOffset;
 
             for (int y = 0; y < height; y++)
             {
-                double x1 = xs;
+                double coordX = xStartPos;
 
                 for (int x = 0; x < width; x++)
                 {
-                    //double a = ExtensionMethods.Remap(x, 0, width, Constants.RangeStart, Constants.RangeEnd);
-                    //double b = ExtensionMethods.Remap(y, 0, height, Constants.RangeStart, Constants.RangeEnd);
-
-                    //double a = ExtensionMethods.Remap(x1, 0, width, rangeStart, rangeEnd);
-                    //double b = ExtensionMethods.Remap(y1, 0, height, rangeStart, rangeEnd);
-
-                    //double a = ExtensionMethods.Remap(x, 0, dX, Constants.RangeStart, Constants.RangeEnd);
-                    //double b = ExtensionMethods.Remap(y, 0, dY, Constants.RangeStart, Constants.RangeEnd);
-
-                    ComplexPoint c = new ComplexPoint(x1, y1);
+                    ComplexPoint c = new ComplexPoint(coordX, coordY);
                     IComplexPoint z = new ComplexPoint(0, 0);
-                    //IComplexPoint z = new ComplexPoint(0, 0);
 
                     int it = 0;
                     do
@@ -68,10 +58,10 @@ namespace MandelbrotSet
 
                     bm.SetPixel(x, y, pixelColor);
 
-                    x1 += xd;
+                    coordX += xOffset;
                 }
 
-                y1 += yd;
+                coordY += yOffset;
             }
 
             return bm;
@@ -79,13 +69,7 @@ namespace MandelbrotSet
 
         public static Bitmap ZoomFractal(Point zoomStart, Point zoomEnd)
         {
-
-            //  TRY ANOTHER APPROACH
-
-            //double lFactorX = (Mandelbrot.posX - Mandelbrot.rangeStart)/(zoomEnd.X - zoomStart.X);
-            //double lFactorY = (Mandelbrot.posX - Mandelbrot.posY) / (zoomEnd.X - zoomStart.X);
-
-
+           
             var mappedStartPoint = ExtensionMethods.MapPoint(zoomStart);
             var mappedEndPont = ExtensionMethods.MapPoint(zoomEnd);
 
